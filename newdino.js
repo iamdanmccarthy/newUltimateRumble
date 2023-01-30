4b22158b8991aa58731736c2c574b44b1cc9d730
Daniel McCarthy <iamdanmccarthy@gmail.com>
2023-01-30 13:16:50 -0500

    updated font sizes

--- newdino.js
+++ newdino.js
@@ -67,7 +67,7 @@ const Brachiosaurus = {
     power: 78
 }
 
-const dinosaurs = [Trex, Ankylosaurus, Spinosaurus, Pachycephalosaurus, Supersaurus, Stegosaurus, Pteradactyl, Velociraptor, Micropachycephalosaurus, Carnotaurus, Parasaurolophus, Triceratops, Dilophosaurus, Brachiosaurus];
+const dinosaurs = [Ankylosaurus, Brachiosaurus, Carnotaurus, Dilophosaurus, Micropachycephalosaurus, Pachycephalosaurus, Parasaurolophus, Pteradactyl, Spinosaurus, Stegosaurus, Supersaurus, Triceratops, Trex, Velociraptor];
 
 let select1 = document.getElementById('dinosaur-one');
 let select2 = document.getElementById('dinosaur-two');
@@ -116,23 +116,13 @@ const ultimateRumble = () => {
         likelyLoser - dinosaur1;
     }
     
-    /*
-    console.log(`${likelyWinner.name} is the likely winner`)
-    console.log(`${likelyLoser.name} is the likely loser`)
-    */
-  
     let num1 = likelyWinner.power;
     let num2 = likelyLoser.power;
 
   
     //determine how much of an advantage the likely winner has
-        let advantageAmount = num1 - num2;
-    //console.log(advantageAmount)
-
+    let advantageAmount = num1 - num2;
     let winningLine = 50 - (50 * (advantageAmount / 100));
-    
-    //console.log(rollOfTheDice);
-    //console.log(winningLine);
 
     //determine winner comparing the roll of the dice against the adjusted "midpoint"
     if (rollOfTheDice >= winningLine) {
@@ -145,70 +135,3 @@ const ultimateRumble = () => {
   
   };
 
-//console.log(ultimateRumble(trex, ankylosaurus));
-
-
-
-//ultimateRumble(firstFighter, secondFighter);
-
-
-
-
-
-/*
-
-let arr = [
-
-  {id: 1,
-  name: 'Dan',
-  age: 42},
-
-  {id: 2,
-    name: 'Jenn',
-    age: 43},
-
-{id: 3,
-  name: 'Rohen',
-  age: 5},
-];
-
-let select1 = document.getElementById('dropdownId1');
-let select2 = document.getElementById('dropdownId2');
-let clicked = document.getElementById('clicked');
-
-for (let obj of arr) {
-  let option = document.createElement("option");
-  option.text = obj.name;
-  option.value = JSON.stringify(obj);
-  select1.add(option);
-}
-
-  for (let obj of arr) {
-    let option = document.createElement("option");
-    option.text = obj.name;
-    option.value = JSON.stringify(obj);
-    select2.add(option);
-}
-
-clicked.onclick = function() {myFunction()}
-
-function myFunction() {
-
-  let selected1 = document.getElementById('dropdownId1').value;
-  let selected2 = document.getElementById('dropdownId2').value;
-  let obj1 = JSON.parse(selected1);
-  let obj2 = JSON.parse(selected2);
-
-
-  if (obj1.age > obj2.age) {
-    console.log(`${obj1.name} is older than ${obj2.name}`)
-  } else  if (obj1.age < obj2.age) {
-    console.log(`${obj2.name} is older than ${obj1.name}`)
-  } else {
-    console.log(`${obj1.name} is the same age as ${obj2.name}`)
-  }
-
-
-};
-
-*/
